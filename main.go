33eac45739beb990151e14d982b0bb23c84ff7cb
Ton van de Ven <threehook@hotmail.com>
2019-03-06 11:29:48 +0100

    Add feat1

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import "fmt"
 
 func main() {
 	fmt.Println("In main")
+	feat1()
 	feat2()
 }
 
+func feat1() {
+	fmt.Println("In feat1")
+}
+
+
 func feat2() {
 	fmt.Println("In feat2")
+	feat1()
 }
+
